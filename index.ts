8c9abee268f8899026a70f562b1dc261dfba6738
Simon Uggla <simon.uggla@gmail.com>
2022-08-17 14:50:45 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import {createServer} from 'http';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+let games = {};
+
+io.on('connection', (socket) => {
+	console.log("user connected");
+});
+
+server.listen(3000, () => {
+  console.log('Running');
+});

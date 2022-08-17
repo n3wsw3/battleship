7c2b0e5dba6b3cecbf26012171fe874bda14b31a
Simon Uggla <simon.uggla@gmail.com>
2022-08-17 14:53:08 +0200

    Fix indentation

--- index.ts
+++ index.ts
@@ -4,13 +4,14 @@ import {Server} from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-
 const io = new Server(server);
 
 let games = {};
 
 io.on('connection', (socket) => {
-	console.log("user connected");
+  console.log("user connected");
+
+  
 });
 
 server.listen(3000, () => {

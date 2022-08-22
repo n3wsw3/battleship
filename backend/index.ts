ac6aadac5ed089e7e6498a8c812ae1ad999f65c7
Simon Uggla <simon.uggla@aol.com>
2022-08-23 01:24:28 +0200

    Moved to use new classes

--- backend/index.ts
+++ backend/index.ts
@@ -1,106 +1,98 @@
-import express from 'express';
-import {createServer} from 'http';
-import {Server} from 'socket.io';
-import {createGame, createPlayer} from './src/game';
-import {
-  getGameFromPlayerId,
-  getGameIdFromPlayerId,
-  getNextPlayerInGame,
-  getPlayerInGame,
-  isShotAHit,
-} from './src/helper';
-import {Games, Ship} from './types';
-import {inspect} from 'util';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { inspect } from "util";
+import { Coord, GameStore, Player, Ship } from "./src/game/store";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
 const PORT = 3000;
 
-type Callback = (payload: {err: string} | {msg: string}) => {};
+type E = [
+  "No Such Game",
+  "Player Not Found",
+  "Game Already Exists",
+  "Not Your Turn",
+  "Cannot Get Next Player"
+]
 
-let games: Games = {};
+type S = [
+  "Game Created Successfully",
+  "Game Joined Successfully",
+  "Added Ships and Readied Up!"
+]
+
+type Errors = E[number];
+type Success = S[number];
+
+type Callback = (payload: { err: Errors } | { msg: Success }) => {};
+
+let games = new GameStore();
 // Outgoing events: [start_game, player_joined, game_finished, shoot]
 // Incoming events: [create, join, shoot, ready_up]
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`Player ${socket.id} connected`);
 
-  socket.on('create', (gameId, onCallback: Callback) => {
-    if (games[gameId]) return onCallback({err: 'Game already exists'});
-    games[gameId] = createGame(gameId, socket.id);
+  socket.on("create", (gameId, onCallback: Callback) => {
+    const game = games.findById(gameId);
+    if (game) return onCallback({ err: "Game Already Exists" });
+    games.create(gameId, socket.id);
     socket.join(gameId);
+
     console.log(`Player ${socket.id} created game ${gameId}`);
     console.log(inspect(games, false, null, true));
-    onCallback({msg: 'Game Created Successfully'});
+
+    onCallback({ msg: "Game Created Successfully" });
   });
 
-  socket.on('join', (gameId, onCallback: Callback) => {
-    if (
-      Object.keys(games).includes(gameId) &&
-      games[gameId].players.length < 2
-    ) {
-      games[gameId].players.push(createPlayer(socket.id));
+  socket.on("join", (gameId, onCallback: Callback) => {
+    const game = games.findById(gameId);
+    if (game && game.players.length < 2) {
+      game.addPlayer(new Player(socket.id));
       socket.join(gameId);
-      socket.to(gameId).emit('player_joined', socket.id);
+      socket.to(gameId).emit("player_joined", socket.id);
 
       console.log(`Player ${socket.id} joined game ${gameId}`);
-      onCallback({msg: 'Gamed Joined Successfully'});
+      onCallback({ msg: "Game Joined Successfully" });
     } else {
       console.warn(`Player ${socket.id} tried to join invalid game ${gameId}`);
-      onCallback({err: 'No Such Game'});
+      onCallback({ err: "No Such Game" });
     }
   });
-  socket.on('shoot', ({x, y}, onCallback: Callback) => {
-    const game = getGameFromPlayerId(games, socket.id);
-    if (!game) return onCallback({err: 'Game not found'});
-    if (game.turn !== socket.id) return onCallback({err: 'Not Your Turn'});
+  socket.on("shoot", ({ x, y }, onCallback: Callback) => {
+    const game = games.getGameFromPlayerId(socket.id);
+    if (!game) return onCallback({ err: "No Such Game" });
 
-    // Update the main game state
-    const player = getPlayerInGame(game, socket.id);
-    if (!player) return onCallback({err: "No Such Player"});
+    if (game.turn !== socket.id) return onCallback({ err: "Not Your Turn" });
 
-    player.shots_fired.push({x, y});
-
-    // Find the id of the next person in the person array and set it to their turn
-    let currentPlayerIndex = game.players.findIndex(
-      (player) => player.socket_id === socket.id
-    );
-    game.turn =
-      game.players[(currentPlayerIndex + 1) % game.players.length].socket_id;
-
-    const nextPlayer = getNextPlayerInGame(game, socket.id);
-    if (!nextPlayer) return onCallback({err: "Cannot Get Next Player"});
-
-    let isHit = isShotAHit({x, y}, nextPlayer.ship);
-    if (isHit) console.log("HIT");
-    
-    // Send the updated shot to all players
-    socket.to(getGameIdFromPlayerId(games, socket.id)).emit('shoot', {x, y});
-
-    onCallback({msg: 'Nice Shot'});
+    const {is_hit, killed_ship} = game.shoot(x, y);
+    io.to(game.game_id).emit("shoot", { x, y, is_hit, killed_ship });
   });
 
-  socket.on('ready_up', (ship: Ship[], onCallback: Callback) => {
-    const game = getGameFromPlayerId(games, socket.id);
-    if (!game) return onCallback({err: 'Game not found'});
+  socket.on("ready_up", (ships: Array<Array<{x: number, y: number}>>, onCallback: Callback) => {
+    const game = games.getGameFromPlayerId(socket.id);
+    if (!game) return onCallback({ err: "No Such Game" });
 
-    const player = getPlayerInGame(game, socket.id);
-    if (!player) return onCallback({err: 'Player not found'});
+    const player = game.getPlayerFromId(socket.id);
+    if (!player) return onCallback({ err: "Player Not Found" });
 
-    player.ship = ship;
+    player.ships = ships.map(ship => new Ship(ship.map(coord => new Coord(coord))));
     player.is_ready = true;
 
-    onCallback({msg: 'Added Ships and Readied Up!'});
+    onCallback({ msg: "Added Ships and Readied Up!" });
 
+    // Start the game if there are more than 2 players and all of
+    // the players are readied up.
     if (
       game.players.length >= 2 &&
       game.players.every((player) => player.is_ready)
     ) {
-      io.to(game.game_id).emit('start_game');
+      io.to(game.game_id).emit("start_game");
     }
   });
 });

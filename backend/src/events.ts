6bb29d2e885f38a2d8b45c0898dfaacb61ef028a
Simon Uggla <simon.uggla@gmail.com>
2022-08-23 16:17:13 +0200

    Added Typings for socket.io

---
+++ backend/src/events.ts
@@ -0,0 +1,33 @@
+import { ErrorsStrings, SuccessStrings } from './types';
+
+interface ShootType {
+  x: number;
+  y: number;
+  is_hit: boolean;
+  killed_ship: boolean;
+}
+
+interface Error<T = ErrorsStrings> {
+  error: T;
+}
+
+type Success<T> = {
+  [K in keyof T]: T[K];
+}
+
+export type ResponseT<T> = Error | Success<T>
+
+export type Response<T = {msg: SuccessStrings}> = (value: ResponseT<T>) => void;
+
+export interface ServerEvents {
+  "shoot": (shot: ResponseT<ShootType>) => void;
+  "player_joined": (playerId: string) => void;
+  "start_game": () => void;
+}
+
+export interface ClientEvents {
+  "create": (game_id: string, onCallback: Response) => void;
+  "join": (game_id: string, onCallback: Response) => void;
+  "ready_up": (ships: Array<Array<{x: number, y: number}>>, onCallback: Response) => void;
+  "shoot": (shot: {x: number, y: number}, onCallback: Response<ShootType>) => void;
+}

b73a431c91d92ce06e89629b0beccd2feab43e15
Simon Uggla <simon.uggla@gmail.com>
2022-08-24 21:04:52 +0200

    Added initial gameboard

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -1,7 +1,18 @@
 <template>
-  <GameBoardBoard />
+  <GameBoardBoard :user_id="user" :ships="ships" :shots="shots" @shoot="shoot"/>
 </template>
 
 <script setup lang="ts">
+import {ICoord} from "backend";
 import GameBoardBoard from "./GameBoardBoard.vue";
+
+const ships = reactive<Array<Array<ICoord>>>([[{x: 1, y: 1}, {x: 1, y: 2}]]);
+const shots = reactive<Array<ICoord>>([{x: 1, y: 1}, {x: 2, y:2}]);
+
+const user = ref("test_user");
+
+const shoot = (coord: ICoord) => {
+  shots.push(coord);
+}
+
 </script>
---
+++ frontend/src/components/GameBoardSquare.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="square"
+    :class="{is_ship: props.has_ship, is_shot: props.is_shot}"
+    @click="emit('shoot')"
+  >
+    {{ props.has_ship ? 1 : 0 }}|{{ props.is_shot ? 1 : 0 }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{is_shot: boolean; has_ship: boolean}>(),
+  {
+    is_shot: false,
+    has_ship: false,
+  }
+);
+
+const emit = defineEmits<{
+  (e: "shoot"): void;
+}>();
+
+</script>
+
+<style>
+.square {
+  --size: 40px;
+  display: inline-block;
+  width: var(--size);
+  height: var(--size);
+  line-height: var(--size);
+  text-align: center;
+  user-select: none;
+  border: 1px solid black;
+}
+
+.square.is_ship {
+  background-color: #5f5f5f;
+}
+
+.square.is_shot {
+  background-color: #5ee6fd;
+}
+
+.square.is_shot.is_ship {
+  background-color: #ff7171;
+}
+
+</style>

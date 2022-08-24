3d75514c6424490f0a9180937b4856eb3b416e2d
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-08-24 09:00:06 +0200

    Add some super cool mega nice ship selection base

--- frontend/src/components/ShipSelectorShip.vue
+++ frontend/src/components/ShipSelectorShip.vue
@@ -1,8 +1,12 @@
 <template>
+  <li>
+    <h4>{{ ship.name.toUpperCase() }}</h4>
+    <img src="#" :alt="`${ship.name} of length ${ship.length}`">
+  </li>
 </template>
 
 <script setup lang="ts">
-import Ship from "../components/ShipSelector.vue";
+import {AvailableShip} from "../types";
 
-const props = defineProps<{ship: typeof Ship}>()
+const props = defineProps<{ship: AvailableShip}>()
 </script>
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,3 +2,10 @@ import { ClientEvents, ServerEvents } from "backend";
 import { Socket as S } from "socket.io-client"
 
 export type Socket = S<ServerEvents, ClientEvents>;
+
+export interface AvailableShip {
+    name: string;
+    length: number;
+    placed: boolean;
+    orientation: "horizontal" | "vertical";
+}

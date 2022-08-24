ad7b4c3423fb97ceb6a0f3db53dfdb7c4f751adc
Simon Uggla <simon.uggla@gmail.com>
2022-08-24 09:17:13 +0200

    Included auto-import type

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from "unplugin-auto-import/vite"
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), AutoImport({
+    imports: ["vue"],
+    resolvers: [
+      ElementPlusResolver()
+    ],
+    dirs: [
+      "./composables",
+    ],
+    vueTemplate: true
+  })],
 })

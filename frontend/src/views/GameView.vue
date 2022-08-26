d9fd04ab85c78bbae50b2287dca1264f6b6a7599
Simon Uggla <simon.uggla@aol.com>
2022-08-26 13:28:23 +0200

    Added placement of ships

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -3,16 +3,18 @@
     <div class="grid grid-cols-3 md:grid-cols-4 h-full">
       <div class="col-span-2 md:col-span-3 flex">
         <GameBoard
+          @shoot="placeShip"
           :ships="ships"
           :shots="shots"
           user_id="You"
-          @shoot="placeShip"
+          :size="gameSize"
         />
         <GameBoard
           @shoot="shoot"
           :ships="otherShips"
           :shots="otherShots"
           :user_id="props.other_player"
+          :size="gameSize"
         />
         <button @click="readyUp">Ready Up</button>
       </div>
@@ -46,6 +48,8 @@ const selectedShip = computed(() => {
   if (selectedShipIndex.value < 0) return;
   return shipsAvailable[selectedShipIndex.value];
 });
+
+const gameSize = reactive<ICoord>({ x: 10, y: 10 });
 
 const ships = reactive<Array<Array<ICoord>>>([
   [
@@ -91,12 +95,25 @@ const shoot = (coord: ICoord) => {
 };
 
 const placeShip = (coord: ICoord) => {
-  if (isReady.value) return console.log("Cannot Place Ship When Ready");
-  if (!selectedShip.value) return console.log("No ship selected");
+  if (isReady.value) return console.log("Cannot Place Ship When Ready!");
+  if (!selectedShip.value) return console.log("No ship selected!");
+  if (selectedShip.value.placed) return console.log("Ship already placed!");
+
+  let shipMapper: (_: any, i: number) => ICoord =
+    selectedShip.value.orientation === "horizontal"
+      ? (_, i) => ({ x: coord.x + i, y: coord.y })
+      : (_, i) => ({ x: coord.x, y: coord.y + 1 });
+
+  let shipPos = Array.from({ length: selectedShip.value.length }, shipMapper);
+
+  if (!shipPos.every(pos => pos.x <= gameSize.x && pos.y <= gameSize.y))
+    return console.log("Trying to place ship outside game board!");
 
   console.log(
     `place ship ${selectedShip.value.name} at ${coord.x}, ${coord.y}`
   );
+  ships.push(shipPos);
+  selectedShip.value.placed = true;
 };
 
 const setShipIndex = (index: number) => {

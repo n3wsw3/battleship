068e842b07b8232bae716b134f3cfc338bdfe7fb
Simon Uggla <simon.uggla@gmail.com>
2022-08-28 17:14:52 +0200

    Changed helper test to be inside describe block

--- tests/backend/helpers.test.ts
+++ tests/backend/helpers.test.ts
@@ -1,11 +1,13 @@
 import { createGameId } from "../../backend/src/helper";
 
-test("Create game id", () => {
-  // Create array with 50 game id's
-  let gameIds = Array(50)
-    .fill(0)
-    .map(() => createGameId());
-  gameIds.forEach(id => {
-    expect(id).toMatch(/^[a-zA-Z0-9]{5}$/);
+describe("Helper methods", () => {
+  test("Create game id", () => {
+    // Create array with 50 game id's
+    let gameIds = Array(50)
+      .fill(0)
+      .map(() => createGameId());
+    gameIds.forEach(id => {
+      expect(id).toMatch(/^[a-zA-Z0-9]{5}$/);
+    });
   });
 });
